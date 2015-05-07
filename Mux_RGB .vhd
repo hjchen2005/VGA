ad8ea3e490b1de0e67a6a8d0b21b8bf2b7da55ab
hjchen2005 <hjchen2005@gmail.com>
2015-05-07 19:39:28 -0400

    Create Mux_RGB .vhd

---
+++ Mux_RGB .vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity Mux_RGB is
+port
+(
+Object_1_On, Video_On: in std_logic;
+Backgroun_RGB, Object_1_RGB : in std_logic_vector(2 downto 0);
+RGB : out std_logic_vector(2 downto 0)
+);
+end Mux_RGB;
+architecture arch of Mux_RGB is
+signal r : std_logic_vector(2 downto 0);
+begin
+--Enter your code here
+process(Object_1_On, Video_On)
+begin
+if(Video_On = '1') then
+if(Object_1_On = '1') then
+RGB <= Object_1_RGB;
+else
+RGB <= Backgroun_RGB;
+end if;
+else
+RGB <= "000";
+end if;
+end process;
+end arch;
